a25dcab2ecff8fa52a914ce24baad6e4ba16dc24
anettgor <anetagorowska06@onet.pl>
2023-01-06 17:21:47 +0100

    preparations for task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,16 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+const calendar = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+
+startBtn.addEventListener('click', () => {});
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
