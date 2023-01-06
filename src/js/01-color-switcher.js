0056c0ef3c2977da05dcb6465483bd5a8139d679
anettgor <anetagorowska06@onet.pl>
2023-01-06 17:12:28 +0100

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,6 @@
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
-const body = document.querySelector('body');
-let timerId = null;
+let timerId;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -9,12 +8,16 @@ function getRandomHexColor() {
 
 const onStart = () => {
   timerId = setInterval(() => {
-    body.style.backgroundColor = getRandomHexColor();
+    document.querySelector('body').style.backgroundColor = getRandomHexColor();
   }, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
 };
 
 const onStop = () => {
-  timerId = setInterval(() => {}, 1000);
+  clearInterval(timerId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
 };
 startBtn.addEventListener('click', onStart);
 stopBtn.addEventListener('click', onStop);
