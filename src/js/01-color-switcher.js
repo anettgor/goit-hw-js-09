4128bd9de3906f105474ba6cdda270313a93f1c9
anettgor <anetagorowska06@onet.pl>
2023-01-06 16:28:17 +0100

    basic preparations and inline color change to body

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const onStart = () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+};
+
+const onStop = () => {
+  timerId = setInterval(() => {}, 1000);
+};
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
