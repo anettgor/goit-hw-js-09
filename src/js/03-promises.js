35e9388e48070ba9e6ebaf8d873ce20aacc827e2
anettgor <anetagorowska06@onet.pl>
2023-01-14 18:16:46 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,8 @@
+const form = document.querySelector('form');
+const firstDelay = form.querySelector('input[name="delay"]');
+const delayStep = form.querySelector('input[name="step"]');
+const delayAmount = form.querySelector('input[name="amount"]');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
